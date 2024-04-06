cd04d98cb03fa768969d183b840ba4d1f1c0bbeb
Ries630 <sugiryo0630@gmail.com>
2024-04-07 00:16:34 +0900

    Create Test Project

---
+++ WinUIApp1.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace WinUIApp1.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        var mainWindow = new MainWindow();
+        mainWindow.Activate();
+
+        
+    }
+}
